7955c7909160bf42ba9191ff60249816ed7022c7
hlatki <hannahatkinson@gmail.com>
2013-12-08 21:53:53 -0500

    Initial commit
    Taking a stab at moving away from OctoPress

---
+++ README.md
@@ -0,0 +1,7 @@
+Uses the [Static](http://nakkaya.com/static.html) static site generator.
+
+To regenerate blog:
+
+```
+java -jar static-app.jar --build
+```
---
+++ config.clj
@@ -0,0 +1,10 @@
+[:site-title "blog.hannah.io"
+ :site-description "Notes on code and such"
+ :site-url "http://blog.hannah.io"
+ :author-name "Hannah Atkinson"
+ :in-dir "resources/"
+ :out-dir "html/"
+ :create-archives true
+ :default-template "default.clj"
+ :encoding "UTF-8"
+ :blog-as-index true]
